cbb72e052f6f36824afe2ca1f247d7bf51d1d47a
Ali Sarchami <ali.sarchami@gmail.com>
2018-03-11 22:04:56 -0500

    modified slave entrypoint to include all master hostnames from their container id

--- mesosslave/entrypoint.sh
+++ mesosslave/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-zk_masters="zk://mesosmaster_1:2181"
-for count in $(seq 2 $NO_OF_MASTERS)
+zk_masters="zk://"
+ids=$(curl -sS --unix-socket /var/run/docker.sock http:/containers/json?all=true | jq '.[] | select(.Image=="mesos_master") | select(.HostConfig["NetworkMode"]=="mesos_default") | .["Id"][:12]')
+
+declare -a arr=("$ids")
+for i in $arr
 do
-    zk_masters=$zk_masters", mesosmaster_"$count":2181"
+	id=$(echo $i | cut -c2- | rev | cut -c2- | rev)
+	zk_masters=$zk_masters"$id:2181,"
 done
-zk_masters=$zk_masters"/mesos"
+zk_masters=$(echo "$zk_masters" | rev | cut -c2- | rev)"/mesos/zk"
 echo $zk_masters > /etc/mesos/zk
 cat /etc/mesos/zk
